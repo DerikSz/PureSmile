703ce9c8fc08f5a8ece030275a14b9c0d38091fd
Derik Batinga <deriksoares2009@gmail.com>
2026-07-30 19:19:08 +0000

    new: Criação da conexão, desconexão e execução dos comandos SQL

---
+++ nomeTemp-ProjetoDS/src/main/java/org/example/connection/connection.java
@@ -0,0 +1,51 @@
+package org.example.connection;
+
+import java.sql.*;
+
+public class connection {
+    final static String driver = "com.mysql.jdbc.Driver";
+    final static String url = "jdbc:mysql://127..0.0.1/clients";
+    final String user = "root";
+    final String password = "root123";
+    private Connection connectionDB;
+    public Statement statement;
+    public ResultSet resultSet;
+
+    public boolean connection () {
+        boolean isConnected = true;
+        try {
+            Class.forName(driver);
+            connectionDB = DriverManager.getConnection("url", user, password);
+            System.out.println("DB connection started");
+
+        }catch(ClassNotFoundException e){
+            System.out.println("Invalid Drive");
+            e.printStackTrace();
+        }
+        catch (SQLException e) {
+            System.out.println("Invalid URL");
+            e.printStackTrace();
+            isConnected = false;
+        }
+        return isConnected;
+    }
+
+    public void desconnection(){
+        try{
+         connectionDB.close();
+            System.out.println("DB Connection Closed.");
+        }catch(SQLException e){
+            System.out.println("DB Connection not found");
+            e.printStackTrace();
+        }
+    }
+    public void SQLexe(String sqlCommand){
+        try{
+          statement = connectionDB.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+          resultSet = statement.executeQuery(sqlCommand);
+        }catch(SQLException e){
+            System.out.println("SQL command invalid!");
+            e.printStackTrace();
+        }
+    }
+}
